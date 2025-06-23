8c9bcc82e79e1e65422778ddb29a86bfc45b776b
Rishitha-1085 <yadlapallirishi23@gmail.com>
2025-06-23 16:37:50 +0530

    Add files via upload

---
+++ zipfunc.py
@@ -0,0 +1,9 @@
+for i,j in zip(range(0,10,1),range(10,0,-1)):
+    print(i,j)
+print("......................................................")
+for i,j in zip(range(1,10,1),range(10,0,-1)):
+    print(i,j)
+print("......................................................")
+for i,j in zip(range(0,5,1),range(10,0,-1)):
+    print(i,j)
+
